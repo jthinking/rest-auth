1d83fe5f426b5de1e99eaec002c32b491907875b
jthinking <jiabochao@live.com>
2017-08-12 09:50:34 +0800

    项目初始化。

---
+++ src/main/java/com/jthinking/util/auth/SecretKeyClient.java
@@ -0,0 +1,28 @@
+package com.jthinking.util.auth;
+
+import java.util.Calendar;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class SecretKeyClient {
+	
+	public static String get() {
+		Calendar calendar = Calendar.getInstance();
+        StringBuilder builder = new StringBuilder();
+        String password = "password";
+        builder.append(password);
+        builder.append(calendar.get(Calendar.YEAR));
+        builder.append(calendar.get(Calendar.MONTH));
+        builder.append(calendar.get(Calendar.DATE));
+        builder.append(calendar.get(Calendar.HOUR));
+        builder.append(calendar.get(Calendar.MINUTE));
+        builder.append(calendar.get(Calendar.DAY_OF_WEEK));
+        builder.append(calendar.get(Calendar.DAY_OF_MONTH));
+        builder.append(calendar.get(Calendar.DAY_OF_YEAR));
+        builder.append(calendar.get(Calendar.WEEK_OF_YEAR));
+        builder.append(calendar.get(Calendar.WEEK_OF_MONTH));
+		String md5Hex = DigestUtils.md5Hex(builder.toString());
+		return md5Hex;
+	}
+
+}
---
+++ src/main/java/com/jthinking/util/auth/SecretKeyServer.java
@@ -0,0 +1,70 @@
+package com.jthinking.util.auth;
+
+import java.util.Calendar;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class SecretKeyServer {
+	
+	private static String[] get() {
+		String[] keys = new String[2];
+		
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int date = calendar.get(Calendar.DATE);
+		int hour = calendar.get(Calendar.HOUR);
+		int minute = calendar.get(Calendar.MINUTE);
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+		int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+		int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
+		int weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
+		int weekOfMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+		String password = "password";
+		
+        StringBuilder builder1 = new StringBuilder();
+        builder1.append(password);
+        builder1.append(year);
+        builder1.append(month);
+        builder1.append(date);
+        builder1.append(hour);
+        builder1.append(minute);
+        builder1.append(dayOfWeek);
+        builder1.append(dayOfMonth);
+        builder1.append(dayOfYear);
+        builder1.append(weekOfYear);
+        builder1.append(weekOfMonth);
+        
+        StringBuilder builder2 = new StringBuilder();
+        builder2.append(password);
+        builder2.append(year);
+        builder2.append(month);
+        builder2.append(date);
+        builder2.append(hour);
+        builder2.append(minute - 1);
+        builder2.append(dayOfWeek);
+        builder2.append(dayOfMonth);
+        builder2.append(dayOfYear);
+        builder2.append(weekOfYear);
+        builder2.append(weekOfMonth);
+        
+		String md5Hex1 = DigestUtils.md5Hex(builder1.toString());
+		String md5Hex2 = DigestUtils.md5Hex(builder2.toString());
+		keys[0] = md5Hex1;
+		keys[1] = md5Hex2;
+		return keys;
+	}
+	
+	public static boolean auth(String key) {
+		String[] keys = get();
+		
+		for (String k : keys) {
+			if (k.equalsIgnoreCase(key)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
